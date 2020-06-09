fdf243adea334ca17979d46fb1ad029ee04ddb74
Jon Manning <jon@secretlab.com.au>
2020-06-09 16:32:26 +1000

    Add support for delivery over time to DialogueRunnerMockUI

--- Tests/Runtime/DialogueRunnerMockUI.cs
+++ Tests/Runtime/DialogueRunnerMockUI.cs
@@ -1,6 +1,7 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using Yarn.Unity;
 
 public class DialogueRunnerMockUI : Yarn.Unity.DialogueViewBase
@@ -11,11 +12,47 @@ public class DialogueRunnerMockUI : Yarn.Unity.DialogueViewBase
     // The text of the most recently received options that we've ben given
     public List<string> CurrentOptions {get;private set;} = new List<string>();
 
+    // The amount of time that this view will take before notifying that
+    // the line has been delivered. If zero, lines are delivered immediately.
+    public float simulatedLineDeliveryTime = 0;
+
+    // The amount of time that this view will take before notifying that
+    // the line has been dismissed. If zero, lines are dismissed
+    // immediately.
+    public float simulatedLineDismissalTime = 0;
+
+    // If true, the line has been interrupted, and we should notify that
+    // delivery is complete immediately.
+    private bool isLineInterrupted;
+
     public override void RunLine(LocalizedLine dialogueLine, Action onLineDeliveryComplete)
     {
-        // Store the localised text in our CurrentLine property and
-        // immediately signal that we're done "delivering" the line
+        // Store the localised text in our CurrentLine property and signal
+        // that we're done "delivering" the line after the correct amount of time
         CurrentLine = dialogueLine.TextLocalized;
+
+        isLineInterrupted = false;
+
+        if (simulatedLineDeliveryTime > 0) {
+            StartCoroutine(SimulateLineDelivery(onLineDeliveryComplete));        
+        } else {
+            onLineDeliveryComplete();
+        }
+        
+    }
+
+    private IEnumerator SimulateLineDelivery(Action onLineDeliveryComplete)
+    {        
+        // Wait for an amount of time before calling the completion handler
+        var lineDeliveryTimeRemaining = simulatedLineDeliveryTime;
+
+        while (lineDeliveryTimeRemaining > 0) {
+            if (isLineInterrupted) {
+                break;
+            }
+            yield return null;
+        }
+        
         onLineDeliveryComplete();
     }
 
@@ -28,12 +65,33 @@ public class DialogueRunnerMockUI : Yarn.Unity.DialogueViewBase
     }
 
     public override void DismissLine(Action onDismissalComplete) {
-        // Immediately indicate that we're done 'dismissing' the line.
+        // Signal that we're done "dismissing" the line after the correct
+        // amount of time
+
+        if (simulatedLineDeliveryTime > 0) {
+            StartCoroutine(SimulateLineDismissal(onDismissalComplete));        
+        } else {
+            onDismissalComplete();
+        }
+    }
+
+    private IEnumerator SimulateLineDismissal(Action onDismissalComplete)
+    {
+        yield return new WaitForSeconds(simulatedLineDismissalTime);
         onDismissalComplete();
     }
 
     public override void OnLineStatusChanged(LocalizedLine dialogueLine)
     {
-        // Do nothing in response to lines changing status
+        switch (dialogueLine.Status)
+        {
+            case LineStatus.Interrupted:
+                isLineInterrupted = true;
+                break;
+            default:
+                // no-op; we don't care about other states in this mock
+                // view
+                break;
+        }        
     }
 }
