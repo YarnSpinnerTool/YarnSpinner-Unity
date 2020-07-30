8d3f0936a707151c0560c4365599310c9a6eb08e
Schroedingers-Cat <6382400+Schroedingers-Cat@users.noreply.github.com>
2020-07-30 12:23:55 +0200

    Add test for a null element in the ViewBase crashing the DialogueRunner

--- Tests/Runtime/DialogueRunnerTests.cs
+++ Tests/Runtime/DialogueRunnerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Linq;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -22,6 +23,39 @@ namespace Yarn.Unity.Tests
 
             var runner = GameObject.FindObjectOfType<DialogueRunner>();
             DialogueRunnerMockUI dialogueUI = GameObject.FindObjectOfType<DialogueRunnerMockUI>();
+
+            runner.StartDialogue();
+            yield return null;
+
+            Assert.That(string.Equals(dialogueUI.CurrentLine, "Spieler: Kannst du mich hören?"));
+            dialogueUI.MarkLineComplete();
+
+            Assert.That(string.Equals(dialogueUI.CurrentLine, "NPC: Klar und deutlich."));
+            dialogueUI.MarkLineComplete();
+
+            Assert.AreEqual(2, dialogueUI.CurrentOptions.Count);
+            Assert.AreEqual("Mir reicht es.", dialogueUI.CurrentOptions[0]);
+            Assert.AreEqual("Nochmal!", dialogueUI.CurrentOptions[1]);
+        }
+
+        [UnityTest]
+        public IEnumerator HandleLine_OnViewsArrayContainingNullElement_SendCorrectLinesToUI()
+        {
+            SceneManager.LoadScene("DialogueRunnerTest");
+            bool loaded = false;
+            SceneManager.sceneLoaded += (index, mode) =>
+            {
+                loaded = true;
+            };
+            yield return new WaitUntil(() => loaded);
+
+            var runner = GameObject.FindObjectOfType<DialogueRunner>();
+            DialogueRunnerMockUI dialogueUI = GameObject.FindObjectOfType<DialogueRunnerMockUI>();
+
+            // Insert a null element into the dialogue views array
+            var viewArrayWithNullElement = runner.dialogueViews.ToList();
+            viewArrayWithNullElement.Add(null);
+            runner.dialogueViews = viewArrayWithNullElement.ToArray();
 
             runner.StartDialogue();
             yield return null;
