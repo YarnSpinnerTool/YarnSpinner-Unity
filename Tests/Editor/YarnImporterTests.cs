48c0b0c39cedba56998f68c4027df268f30ea06a
Jon Manning <jon@secretlab.com.au>
2020-08-08 00:45:04 +1000

    Slightly nicer cleanup in YarnImporterTests

--- Tests/Editor/YarnImporterTests.cs
+++ Tests/Editor/YarnImporterTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using NUnit.Framework;
 using UnityEditor;
@@ -10,19 +11,36 @@ namespace Yarn.Unity.Tests
 {
     public class YarnImporterTests
     {
+        List<string> createdFilePaths = new List<string>();
+
+        [TearDown]
+        public void TearDown() {
+            foreach (var path in createdFilePaths) {
+                File.Delete(path);
+
+                string metaFilePath = path + ".meta";
+
+                if (File.Exists(metaFilePath)) {
+                    File.Delete(metaFilePath);
+                }
+            }
+        }
+
         [Test]
         public void YarnImporter_OnValidYarnFile_ShouldCompile()
         {
             const string textYarnAsset = "title: Start\ntags:\ncolorID: 0\nposition: 0,0\n--- \nSpieler: Kannst du mich hören? #line:0e3dc4b\nNPC: Klar und deutlich. #line:0967160\n[[Mir reicht es.| Exit]] #line:04e806e\n[[Nochmal!|Start]] #line:0901fb2\n===\ntitle: Exit\ntags: \ncolorID: 0\nposition: 0,0\n--- \n===";
             string fileName = Path.GetRandomFileName();
 
-            File.WriteAllText(Application.dataPath + "/" + fileName + ".yarn", textYarnAsset);
+            var path = Application.dataPath + "/" + fileName + ".yarn";
+            createdFilePaths.Add(path);
+
+            File.WriteAllText(path, textYarnAsset);
             AssetDatabase.Refresh();
             var result = ScriptedImporter.GetAtPath("Assets/" + fileName + ".yarn") as YarnImporter;
 
             Assert.That(result.isSuccesfullyCompiled);
 
-            AssetDatabase.DeleteAsset("Assets/" + fileName + ".yarn");
         }
 
         [Test]
@@ -31,15 +49,16 @@ namespace Yarn.Unity.Tests
             const string textYarnAsset = "This is not a valid yarn file and thus compilation should fail.";
             string fileName = Path.GetRandomFileName();
 
-            File.WriteAllText(Application.dataPath + "/" + fileName + ".yarn", textYarnAsset);
+            string path = Application.dataPath + "/" + fileName + ".yarn";
+            createdFilePaths.Add(path);
+            File.WriteAllText(path, textYarnAsset);
+
             LogAssert.ignoreFailingMessages = true;
             AssetDatabase.Refresh();
             LogAssert.ignoreFailingMessages = false;
             var result = ScriptedImporter.GetAtPath("Assets/" + fileName + ".yarn") as YarnImporter;
 
             Assert.That(!result.isSuccesfullyCompiled);
-
-            AssetDatabase.DeleteAsset("Assets/" + fileName + ".yarn");
         }
 
         [Test]
@@ -47,15 +66,15 @@ namespace Yarn.Unity.Tests
         {
             const string textYarnAsset = "title: Start\ntags:\ncolorID: 0\nposition: 0,0\n--- \nSpieler: Kannst du mich hören? #line:0e3dc4b\nNPC: Klar und deutlich. #line:0967160\n[[Mir reicht es.| Exit]] #line:04e806e\n[[Nochmal!|Start]] #line:0901fb2\n===\ntitle: Exit\ntags: \ncolorID: 0\nposition: 0,0\n--- \n===";
             string fileName = Path.GetRandomFileName();
-            string expectedStringTable = "id,text,file,node,lineNumber\r\nline:0e3dc4b,Spieler: Kannst du mich hören?," + fileName + ",Start,6\r\nline:0967160,NPC: Klar und deutlich.," + fileName + ",Start,7\r\nline:04e806e,Mir reicht es.," + fileName + ",Start,8\r\nline:0901fb2,Nochmal!," + fileName + ",Start,9\r\n";
+            string path = Application.dataPath + "/" + fileName + ".yarn";
+            createdFilePaths.Add(path);
 
-            File.WriteAllText(Application.dataPath + "/" + fileName + ".yarn", textYarnAsset);
+            File.WriteAllText(path, textYarnAsset);
             AssetDatabase.Refresh();
             var result = ScriptedImporter.GetAtPath("Assets/" + fileName + ".yarn") as YarnImporter;
 
             Assert.That(Equals(result.baseLanguage.text, expectedStringTable));
 
-            AssetDatabase.DeleteAsset("Assets/" + fileName + ".yarn");
         }
 
         [Test]
