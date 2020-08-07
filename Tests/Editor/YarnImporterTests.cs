74d3dd188e6db47197309bc0cb33ea0f5b99cac4
Jon Manning <jon@secretlab.com.au>
2020-08-08 00:46:46 +1000

    Make YarnImporter_OnValidYarnFile_GetExpectedStrings pass again

--- Tests/Editor/YarnImporterTests.cs
+++ Tests/Editor/YarnImporterTests.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using NUnit.Framework;
 using UnityEditor;
 using UnityEditor.Experimental.AssetImporters;
@@ -66,6 +67,42 @@ namespace Yarn.Unity.Tests
         {
             const string textYarnAsset = "title: Start\ntags:\ncolorID: 0\nposition: 0,0\n--- \nSpieler: Kannst du mich hören? #line:0e3dc4b\nNPC: Klar und deutlich. #line:0967160\n[[Mir reicht es.| Exit]] #line:04e806e\n[[Nochmal!|Start]] #line:0901fb2\n===\ntitle: Exit\ntags: \ncolorID: 0\nposition: 0,0\n--- \n===";
             string fileName = Path.GetRandomFileName();
+
+            var expectedStrings = new List<StringTableEntry>() {
+                new StringTableEntry {
+                    Language = YarnImporter.DefaultLocalizationName,
+                    ID = "line:0e3dc4b",
+                    Text = "Spieler: Kannst du mich hören?",
+                    File = fileName,
+                    Node = "Start",
+                    LineNumber = "6",                    
+                },
+                new StringTableEntry {
+                    Language = YarnImporter.DefaultLocalizationName,
+                    ID = "line:0967160",
+                    Text = "NPC: Klar und deutlich.",
+                    File = fileName,
+                    Node = "Start",
+                    LineNumber = "7",                    
+                },
+                new StringTableEntry {
+                    Language = YarnImporter.DefaultLocalizationName,
+                    ID = "line:04e806e",
+                    Text = "Mir reicht es.",
+                    File = fileName,
+                    Node = "Start",
+                    LineNumber = "8",                    
+                },
+                new StringTableEntry {
+                    Language = YarnImporter.DefaultLocalizationName,
+                    ID = "line:0901fb2",
+                    Text = "Nochmal!",
+                    File = fileName,
+                    Node = "Start",
+                    LineNumber = "9",                    
+                }
+            };
+
             string path = Application.dataPath + "/" + fileName + ".yarn";
             createdFilePaths.Add(path);
 
@@ -73,8 +110,19 @@ namespace Yarn.Unity.Tests
             AssetDatabase.Refresh();
             var result = ScriptedImporter.GetAtPath("Assets/" + fileName + ".yarn") as YarnImporter;
 
-            Assert.That(Equals(result.baseLanguage.text, expectedStringTable));
+            // Importing this Yarn script will have produced a CSV
+            // TextAsset containing the string table extracted from this
+            // script. Parse that - we'll check that it contains what we
+            // expect.
+            var generatedStringsTable = StringTableEntry.ParseFromCSV(result.baseLanguage.text);
 
+            // Simplify the results so that we can compare these string
+            // table entries based only on specific fields
+            System.Func<StringTableEntry, (string id, string text)> simplifier = e => (id: e.ID, text: e.Text);
+            var simpleResult = expectedStrings.Select(simplifier);
+            var simpleExpected = generatedStringsTable.Select(simplifier);
+
+            Assert.AreEqual(simpleExpected, simpleResult);            
         }
 
         [Test]
