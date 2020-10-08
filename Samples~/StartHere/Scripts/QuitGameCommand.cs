35085b6e95e25d43efc16f967ee267e60e463781
Jon Manning <jon@secretlab.com.au>
2020-10-08 22:58:18 +1100

    Update intro yarn file. Add quit command.

---
+++ Samples~/StartHere/Scripts/QuitGameCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class QuitGameCommand : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<Yarn.Unity.DialogueRunner>().AddCommandHandler("quit", () => Quit());
+    }
+
+    private void Quit()
+    {
+        // In the editor, leave Play Mode. In a regular build, quit the
+        // app.
+        #if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif        
+    }
+}
