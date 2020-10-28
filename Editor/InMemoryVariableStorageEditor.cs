458228c100fd4d2e35e30afcab61328804c6c2e4
Robert Yang <2285943+radiatoryang@users.noreply.github.com>
2020-10-28 17:08:08 -0400

    oops forgot to put InMemoryVariableStorageEditor in Yarn.Unity namespace

--- Editor/InMemoryVariableStorageEditor.cs
+++ Editor/InMemoryVariableStorageEditor.cs
@@ -2,31 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using Yarn.Unity;
 
-[CustomEditor(typeof(InMemoryVariableStorage))]
-public class InMemoryVariableStorageEditor : Editor 
+namespace Yarn.Unity
 {
-    public override void OnInspectorGUI()
+
+    [CustomEditor(typeof(InMemoryVariableStorage))]
+    public class InMemoryVariableStorageEditor : Editor 
     {
-        base.OnInspectorGUI();
-        var varStorage = (InMemoryVariableStorage)target;
-        
-        varStorage.showDebug = EditorGUILayout.Foldout(varStorage.showDebug, "Debug Variables");
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            var varStorage = (InMemoryVariableStorage)target;
+            
+            varStorage.showDebug = EditorGUILayout.Foldout(varStorage.showDebug, "Debug Variables");
 
-        if ( !varStorage.showDebug ) {
-            return;
+            if ( !varStorage.showDebug ) {
+                return;
+            }
+
+            if ( !Application.isPlaying ) {
+                EditorGUILayout.HelpBox("Not in Play Mode, so no variables to display!", MessageType.Info);
+                return;
+            }
+
+            var style = EditorGUIUtility.GetBuiltinSkin( EditorSkin.Inspector ).label;
+            var list = varStorage.GetDebugList();
+            var height = style.CalcHeight( new GUIContent(list), EditorGUIUtility.currentViewWidth ) - 5;
+            EditorGUILayout.SelectableLabel( list, GUILayout.MaxHeight(height), GUILayout.ExpandHeight(true) );
+            
         }
-
-        if ( !Application.isPlaying ) {
-            EditorGUILayout.HelpBox("Not in Play Mode, so no variables to display!", MessageType.Info);
-            return;
-        }
-
-        var style = EditorGUIUtility.GetBuiltinSkin( EditorSkin.Inspector ).label;
-        var list = varStorage.GetDebugList();
-        var height = style.CalcHeight( new GUIContent(list), EditorGUIUtility.currentViewWidth ) - 5;
-        EditorGUILayout.SelectableLabel( list, GUILayout.MaxHeight(height), GUILayout.ExpandHeight(true) );
-        
     }
+
 }
