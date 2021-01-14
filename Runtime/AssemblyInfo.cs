ff9e2cd179cfffa350336357d9db7caac15c2feb
Jon Manning <jon@secretlab.com.au>
2021-01-14 15:45:23 +1100

    Update assembly info for runtime and editor DLLs

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyVersion("2.0")]
+[assembly: AssemblyInformationalVersion("2.0.0-beta2")]
 [assembly: InternalsVisibleTo("YarnSpinnerTests")]
