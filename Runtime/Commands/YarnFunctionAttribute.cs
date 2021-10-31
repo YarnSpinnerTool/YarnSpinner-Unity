d6669b2887b1ca7343eed2d98e234ddf4dfaed64
Shane Duan <git@xsduan.com>
2021-10-30 23:46:20 -0500

    Add more documentation

--- Runtime/Commands/YarnFunctionAttribute.cs
+++ Runtime/Commands/YarnFunctionAttribute.cs
@@ -9,6 +9,14 @@ namespace Yarn.Unity
     /// Marks the method as a function to be registered with the running
     /// instance's library.
     /// </summary>
+    /// <remarks>
+    /// See <see cref="Library.RegisterFunction(string, Delegate)"/> and the
+    /// generic overloads for what is and is not valid.
+    /// 
+    /// This will throw an error if you attempt to add a function that has
+    /// more than 16 parameters, as that is the largest overload that
+    /// <see cref="Func{TResult}"/> etc has.
+    /// </remarks>
     public class YarnFunctionAttribute : YarnActionAttribute {
         [Obsolete("Use " + nameof(Name) + " instead.")]
         public string FunctionName
