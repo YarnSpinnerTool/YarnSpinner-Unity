ad74133b46557c7807c2752fb006576fbcce650b
Shane Duan <git@xsduan.com>
2021-11-03 17:12:40 -0500

    Add Yarn node inspector
    Fixes #29

---
+++ Runtime/YarnPropertyDrawers.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Linq;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using System.Collections.Generic;
+using UnityEditor;
+#endif
+
+namespace Yarn.Unity
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class YarnNodeAttribute : PropertyAttribute
+    {
+        public string Project { get; set; }
+
+        public YarnNodeAttribute(string project) => Project = project;
+    }
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(YarnNodeAttribute))]
+    public class YarnNodesDrawer : PropertyDrawer
+    {
+        private string[] GetNodes(YarnProject project)
+        {
+            return project.GetProgram().Nodes.Keys.ToArray();
+        }
+
+        private YarnProject GetYarnProject(SerializedProperty property)
+        {
+            var projectFieldName = (attribute as YarnNodeAttribute)?.Project;
+            if (projectFieldName == null) { return null; }
+
+            return property.serializedObject.FindProperty(projectFieldName).objectReferenceValue as YarnProject;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var project = GetYarnProject(property);
+            if (project == null || property.propertyType != SerializedPropertyType.String) 
+            {
+                EditorGUI.PropertyField(position, property, label);
+                return;
+            }
+
+            var nodes = GetNodes(project);
+            int index = Math.Max(0, Array.IndexOf(nodes, property.stringValue));
+            int selected = EditorGUI.Popup(
+                position,
+                new GUIContent(property.displayName, property.tooltip),
+                index,
+                nodes.Select(node => new GUIContent(node)).ToArray());
+            property.stringValue = nodes[selected];
+        }
+    }
+#endif
+}
