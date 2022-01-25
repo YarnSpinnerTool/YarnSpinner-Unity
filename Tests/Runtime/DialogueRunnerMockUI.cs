ed4b7164777a57e0cec1787867abfbf40b388c20
Tim Nugent <tim@lonely.coffee>
2022-01-25 12:25:48 +1100

    Removed a lot of timing from mock UI
    As this is a view that exists for the purpose of testing that the runner, and not the view, having it have timing elements seems to just add complexity and bug potential.

--- Tests/Runtime/DialogueRunnerMockUI.cs
+++ Tests/Runtime/DialogueRunnerMockUI.cs
@@ -22,20 +22,6 @@ namespace Yarn.Unity.Tests
         // given
         public List<string> CurrentOptions { get; private set; } = new List<string>();
 
-        // The amount of time that this view will take before notifying
-        // that the line has been delivered. If zero, lines are delivered
-        // immediately.
-        public float simulatedLineDeliveryTime = 0;
-
-        // The amount of time that this view will take before notifying
-        // that the line has been dismissed. If zero, lines are dismissed
-        // immediately.
-        public float simulatedLineDismissalTime = 0;
-
-        // If true, the line has been interrupted, and we should notify
-        // that delivery is complete immediately.
-        private bool isLineInterrupted;
-
         private void Awake()
         {
             instance = this;
@@ -52,39 +38,10 @@ namespace Yarn.Unity.Tests
         public override void RunLine(LocalizedLine dialogueLine, Action onLineDeliveryComplete)
         {
             // Store the localised text in our CurrentLine property and
-            // signal that we're done "delivering" the line after the
-            // correct amount of time
+            // capture the completion handler so it can be called at
+            // the correct moment later by the test system
             CurrentLine = dialogueLine.Text.Text;
             lineDelivered = onLineDeliveryComplete;
-
-            isLineInterrupted = false;
-
-            // if (simulatedLineDeliveryTime > 0)
-            // {
-            //     StartCoroutine(SimulateLineDelivery(onLineDeliveryComplete));
-            // }
-            // else
-            // {
-            //     onLineDeliveryComplete();
-            // }
-        }
-
-        private IEnumerator SimulateLineDelivery(Action onLineDeliveryComplete)
-        {
-            // Wait for an amount of time before calling the completion
-            // handler
-            var lineDeliveryTimeRemaining = simulatedLineDeliveryTime;
-
-            while (lineDeliveryTimeRemaining > 0)
-            {
-                if (isLineInterrupted)
-                {
-                    break;
-                }
-                yield return null;
-            }
-
-            onLineDeliveryComplete();
         }
 
         public override void RunOptions(DialogueOption[] dialogueOptions, Action<int> onOptionSelected)
@@ -98,28 +55,11 @@ namespace Yarn.Unity.Tests
 
         public override void DismissLine(Action onDismissalComplete)
         {
-            // Signal that we're done "dismissing" the line after the
-            // correct amount of time
-
-            if (simulatedLineDeliveryTime > 0)
-            {
-                StartCoroutine(SimulateLineDismissal(onDismissalComplete));
-            }
-            else
-            {
-                onDismissalComplete();
-            }
-        }
-
-        private IEnumerator SimulateLineDismissal(Action onDismissalComplete)
-        {
-            yield return new WaitForSeconds(simulatedLineDismissalTime);
             onDismissalComplete();
         }
 
         public override void InterruptLine(LocalizedLine dialogueLine, Action onDialogueLineFinished)
         {
-            isLineInterrupted = true;
             onDialogueLineFinished();
         }
 
