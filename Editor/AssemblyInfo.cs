00e89b79121e86ffd7dd0ea8ac5b27f55cd44919
Jon Manning <jon@secretlab.com.au>
2022-10-31 13:09:46 +1100

    Update package versions [skip ci]

--- Editor/AssemblyInfo.cs
+++ Editor/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("2.2.1.0")]
+[assembly: AssemblyVersion("2.2.2.0")]
 [assembly: InternalsVisibleTo("YarnSpinner.Editor.Tests")]
 
-[assembly: AssemblyFileVersion("2.2.1.0")]
-[assembly: AssemblyInformationalVersion("2.2.1.0")]
+[assembly: AssemblyFileVersion("2.2.2.0")]
+[assembly: AssemblyInformationalVersion("2.2.2.0")]
