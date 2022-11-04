f564b60f0a848ede47fdf868dc17a4c0d8c4d7eb
Jon Manning <jon@secretlab.com.au>
2022-11-04 15:53:02 +1100

    Offer to add unity line provider if needed

--- Editor/Editors/DialogueRunnerEditor.cs
+++ Editor/Editors/DialogueRunnerEditor.cs
@@ -56,8 +56,44 @@ namespace Yarn.Unity.Editor
 
             if (lineProviderProperty.objectReferenceValue == null)
             {
+                var yarnProject = yarnProjectProperty.objectReferenceValue as YarnProject;
                 EditorGUI.indentLevel += 1;
-                EditorGUILayout.HelpBox($"A {ObjectNames.NicifyVariableName(nameof(TextLineProvider))} component will be added at run time.", MessageType.Info);
+                if (yarnProject != null && yarnProject.localizationType == LocalizationType.Unity) {
+#if USE_UNITY_LOCALIZATION && YARN_ENABLE_EXPERIMENTAL_FEATURES
+                    // If this is a project using Unity localisation, we can't add a
+                    // line provider at runtime because we won't know what string
+                    // table it should use. In this situation, we'll show a warning
+                    // and offer a quick button they can click to add one.
+                    string unityLocalizedLineProvider = ObjectNames.NicifyVariableName(nameof(UnityLocalization.UnityLocalisedLineProvider));
+                    EditorGUILayout.HelpBox($"This project uses Unity Localization. You will need to add a {unityLocalizedLineProvider} for it to work. Click the button below to add one, and then set it up.", MessageType.Warning);
+
+                    GameObject gameObject = (serializedObject.targetObject as DialogueRunner).gameObject;
+
+                    UnityLocalization.UnityLocalisedLineProvider existingLineProvider = gameObject.GetComponent<UnityLocalization.UnityLocalisedLineProvider>();
+
+                    if (existingLineProvider != null) {
+                        if (GUILayout.Button($"Use {unityLocalizedLineProvider}")) {
+                            lineProviderProperty.objectReferenceValue = existingLineProvider;
+                        }
+                    } else {
+                        if (GUILayout.Button($"Add {unityLocalizedLineProvider}")) {
+                            var lineProvider = gameObject.AddComponent<UnityLocalization.UnityLocalisedLineProvider>();
+
+                            lineProviderProperty.objectReferenceValue = lineProvider;
+                        }
+                    }
+                    
+                    #else
+                    EditorGUILayout.HelpBox($"This project uses Unity Localization, but Unity Localization is not installed. Please install it, or change this Yarn Project to use Yarn Spinner's internal localisation system.", MessageType.Error);
+                    #endif
+
+                } else {
+                    // Otherwise, we'll assume they're using the built-in
+                    // localisation system, and we can safely create one at
+                    // runtime because we know everything we need to to set that
+                    // up.
+                    EditorGUILayout.HelpBox($"A {ObjectNames.NicifyVariableName(nameof(TextLineProvider))} component will be added at run time.", MessageType.Info);
+                }
                 EditorGUI.indentLevel -= 1;
             }
 
