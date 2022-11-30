fe66dd1f8707cb59d26aa496254496d7575431b7
Peter Ruibal <ruibalp@gmail.com>
2022-11-29 20:15:04 -0800

    Memoize GetElementHeight()
    On a project with ~300 variables, I am seeing GetElementHeight get
    called *thousands* of times per interaction.  Memoizing this call
    reduces repaint time from ~10 seconds down to 0.3 seconds on this
    project.

--- Editor/Importers/YarnProjectImporter.cs
+++ Editor/Importers/YarnProjectImporter.cs
@@ -840,6 +840,8 @@ namespace Yarn.Unity.Editor
         private SerializedObject serializedObject;
         private SerializedProperty serializedProperty;
 
+        private Dictionary<int, float> _lineHeights = new Dictionary<int, float>();
+
         public ReorderableDeclarationsList(SerializedObject serializedObject, SerializedProperty property)
         {
             this.serializedObject = serializedObject;
@@ -953,8 +955,12 @@ namespace Yarn.Unity.Editor
 
             if (useSearch == false || ShouldShowElement(index))
             {
-                var item = serializedProperty.GetArrayElementAtIndex(index);
-                return DeclarationPropertyDrawer.GetPropertyHeightImpl(item, null);
+                if (!_lineHeights.ContainsKey(index))
+                {
+                    var item = serializedProperty.GetArrayElementAtIndex(index);
+                    _lineHeights[index] = DeclarationPropertyDrawer.GetPropertyHeightImpl(item, null);
+                }
+                return _lineHeights[index];
             }
             else
             {
@@ -978,6 +984,7 @@ namespace Yarn.Unity.Editor
 
         public void DrawLayout()
         {
+            _lineHeights.Clear();
             //serializedObject.Update();
             EditorGUILayout.Space();
 
