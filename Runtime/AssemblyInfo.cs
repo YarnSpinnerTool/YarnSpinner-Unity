6c4ce5db97e10d91543d98e2aaa06d557bc7992f
Jon Manning <jon@secretlab.com.au>
2023-01-27 14:16:13 +1100

    Update AssemblyInfo for v2.2.4

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("2.2.3.0")]
-[assembly: AssemblyFileVersion("2.2.3.0")]
-[assembly: AssemblyInformationalVersion("2.2.3")]
+[assembly: AssemblyVersion("2.2.4.0")]
+[assembly: AssemblyFileVersion("2.2.4.0")]
+[assembly: AssemblyInformationalVersion("2.2.4")]
 [assembly: InternalsVisibleTo("YarnSpinnerTests")]
 [assembly: InternalsVisibleTo("YarnSpinner.Editor.Tests")]
 [assembly: InternalsVisibleTo("YarnSpinner.Unity.Editor")]
--- Samples~/AssemblyInfo.cs
+++ Samples~/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.2.1.0")]
-
-[assembly: AssemblyFileVersion("2.2.1.0")]
-[assembly: AssemblyInformationalVersion("2.2.1")]
+[assembly: AssemblyVersion("2.2.4.0")]
+[assembly: AssemblyFileVersion("2.2.4.0")]
+[assembly: AssemblyInformationalVersion("2.2.4")]
