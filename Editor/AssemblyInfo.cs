fc9e59673f078064bf1c640b0783b6f7fb77606b
Jon Manning <jon@secretlab.com.au>
2023-01-27 14:16:13 +1100

    Update AssemblyInfo for v2.2.4

--- Editor/AssemblyInfo.cs
+++ Editor/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("2.2.3.0")]
-[assembly: InternalsVisibleTo("YarnSpinner.Editor.Tests")]
+[assembly: AssemblyVersion("2.2.4.0")]
+[assembly: AssemblyFileVersion("2.2.4.0")]
+[assembly: AssemblyInformationalVersion("2.2.4")]
 
-[assembly: AssemblyFileVersion("2.2.3.0")]
-[assembly: AssemblyInformationalVersion("2.2.3.0")]
+[assembly: InternalsVisibleTo("YarnSpinner.Editor.Tests")]
