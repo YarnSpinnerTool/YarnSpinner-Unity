304577bacaf620b770801b98e11c930cf40a6331
Jon Manning <jon@secretlab.com.au>
2023-11-14 16:12:25 +1100

    Update version to v2.4.0

--- Samples~/AssemblyInfo.cs
+++ Samples~/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.3.1.0")]
-[assembly: AssemblyFileVersion("2.3.1.0")]
-[assembly: AssemblyInformationalVersion("2.3.1")]
+[assembly: AssemblyVersion("2.4.0.0")]
+[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyInformationalVersion("2.4.0")]
