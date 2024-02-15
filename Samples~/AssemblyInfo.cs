0d577224fa7709e2155d1454a08b003d4e576bc7
Jon Manning <jon@secretlab.com.au>
2024-02-15 15:01:33 +1100

    Update assembly and package version to 2.4.2

--- Samples~/AssemblyInfo.cs
+++ Samples~/AssemblyInfo.cs
@@ -4,6 +4,6 @@ Yarn Spinner is licensed to you under the terms found in the file LICENSE.md.
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.4.0.0")]
-[assembly: AssemblyFileVersion("2.4.0.0")]
-[assembly: AssemblyInformationalVersion("2.4.0")]
+[assembly: AssemblyVersion("2.4.2.0")]
+[assembly: AssemblyFileVersion("2.4.2.0")]
+[assembly: AssemblyInformationalVersion("2.4.2")]
