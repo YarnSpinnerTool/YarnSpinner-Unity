5e3851c2b24a9d351d666e9a80883dc4b2bed565
Jon Manning <jon@secretlab.com.au>
2024-02-16 12:12:21 +1100

    Update version number to 3.0.0

--- Editor/AssemblyInfo.cs
+++ Editor/AssemblyInfo.cs
@@ -5,9 +5,9 @@ Yarn Spinner is licensed to you under the terms found in the file LICENSE.md.
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("2.4.2.0")]
-[assembly: AssemblyFileVersion("2.4.2.0")]
-[assembly: AssemblyInformationalVersion("2.4.2")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0")]
 
 [assembly: InternalsVisibleTo("YarnSpinner.Unity.Editor")]
 [assembly: InternalsVisibleTo("YarnSpinner.Editor.Tests")]
