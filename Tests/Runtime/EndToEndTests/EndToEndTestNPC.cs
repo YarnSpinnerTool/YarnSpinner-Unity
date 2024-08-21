4cb4838f175c56822cb98fe2f5ae04d1198ba907
Jon Manning <jon@secretlab.com.au>
2024-08-21 16:17:23 +1000

    Update EndToEndTest

--- Tests/Runtime/EndToEndTests/EndToEndTestNPC.cs
+++ Tests/Runtime/EndToEndTests/EndToEndTestNPC.cs
@@ -7,13 +7,17 @@ using UnityEngine;
 #nullable enable
 
 namespace Yarn.Unity.Tests {
+    [ExecuteAlways]
     public class EndToEndTestNPC : MonoBehaviour {
         public string? nodeName = null;
 
         public void Awake() {
             var label = this.GetComponentInChildren<TMPro.TMP_Text>();
-            if (label != null) {
+            if (label != null && label.text != this.nodeName) {
                 label.text = this.nodeName;
+                #if UNITY_EDITOR
+                UnityEditor.EditorUtility.SetDirty(label.gameObject);
+                #endif
             }
         }
     }
