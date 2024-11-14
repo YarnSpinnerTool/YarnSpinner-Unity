e9eece0fba6e6f75ae5f6052a8c7f3f46e210917
Jon Manning <jon@secretlab.com.au>
2024-11-14 23:24:28 +1100

    Document LineProviderBase

--- Runtime/LineProviders/LineProviderBase.cs
+++ Runtime/LineProviders/LineProviderBase.cs
@@ -24,15 +24,101 @@ using YarnLineTask = System.Threading.Tasks.Task<Yarn.Unity.LocalizedLine>;
 
 #nullable enable
 
+/// <summary>
+/// Contains methods for retrieving user-facing localized content, given
+/// non-localized line IDs.
+/// </summary>
 public interface ILineProvider
 {
+    /// <summary>
+    /// The <see cref="YarnProject"/> that contains the localized data for
+    /// lines.
+    /// </summary>
+    /// <remarks>
+    /// This property is set at run-time by the object that will be requesting
+    /// content (typically a <see cref="DialogueRunner"/>).
+    /// </remarks>
     public YarnProject? YarnProject { get; set; }
+
+    /// <summary>
+    /// Gets the user's current locale identifier, as a BCP-47 code.
+    /// </summary>
+    /// <remarks>
+    /// This value is used to control how certain replacement markers behave
+    /// (for example, the <c>[plural]</c> marker, which behaves differently
+    /// depending on the user's locale.)
+    /// </remarks>
     public string LocaleCode { get; }
+
+    /// <summary>
+    /// Prepares and returns a <see cref="LocalizedLine"/> from the specified
+    /// <see cref="Yarn.Line"/>.
+    /// </summary>
+    /// <remarks>
+    /// This method should not be called if <see cref="LinesAvailable"/> returns
+    /// <see langword="false"/>.
+    /// </remarks>
+    /// <param name="line">The <see cref="Yarn.Line"/> to produce the <see
+    /// cref="LocalizedLine"/> from.</param>
+    /// <param name="cancellationToken">A cancellation token that indicates
+    /// whether the process of fetching the localised version of <paramref
+    /// name="line"/> should be cancelled.</param>
+    /// <returns>A localized line, ready to be presented to the
+    /// player.</returns>
     public YarnLineTask GetLocalizedLineAsync(Line line, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Signals to the line provider that lines with the provided line IDs may
+    /// be presented shortly.        
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This method allows implementing classes a chance to prepare any
+    /// neccessary resources needed to present these lines, like pre-loading
+    /// voice-over audio. The default implementation does nothing.
+    /// </para>
+    /// <para style="info">
+    /// Not every line may run; this method serves as a way to give the line
+    /// provider advance notice that a line <i>may</i> run, not <i>will</i> run.
+    /// </para>
+    /// <para>
+    /// When this method is run, the value returned by the <see
+    /// cref="LinesAvailable"/> property should change to false until the
+    /// necessary resources have loaded.
+    /// </para>
+    /// </remarks>
+    /// <param name="lineIDs">A collection of line IDs that the line provider
+    /// should prepare for.</param>
+    /// <param name="cancellationToken">A cancellation token that indicates
+    /// whether the operation should be cancelled.</param>
     public YarnTask PrepareForLinesAsync(IEnumerable<string> lineIDs, CancellationToken cancellationToken);
 
+    /// <summary>
+    /// Adds a new marker processor to the line provider.
+    /// </summary>
+    /// <param name="attributeName">The name of the markers to use <paramref
+    /// name="markerProcessor"/> for.</param>
+    /// <param name="markerProcessor">The marker processor to add.</param>
     public void RegisterMarkerProcessor(string attributeName, Yarn.Markup.IAttributeMarkerProcessor markerProcessor);
+
+    /// <summary>
+    /// Removes all marker processors that handle markers named <paramref
+    /// name="attributeName"/>.
+    /// </summary>
+    /// <param name="attributeName">The name of the marker to remove processors
+    /// for.</param>
     public void DeregisterMarkerProcessor(string attributeName);
+
+    /// <summary>
+    /// Gets a value indicating whether the line provider is currently ready to
+    /// produce lines.
+    /// </summary>
+    /// <remarks>
+    /// If this value is <see langword="false"/>, <see
+    /// cref="GetLocalizedLineAsync(Line, CancellationToken)"/> is expected to
+    /// take longer than usual.
+    /// </remarks>
+    public bool LinesAvailable { get; }
 }
 
 
@@ -43,85 +129,30 @@ namespace Yarn.Unity
     /// cref="LocalizedLine"/>s, for use in Dialogue Views.
     /// </summary>
     /// <remarks>
-    /// <para>
     /// <see cref="DialogueRunner"/>s use a <see cref="LineProviderBehaviour"/>
     /// to get <see cref="LocalizedLine"/>s, which contain the localized
-    /// information that <see cref="DialogueViewBase"/> classes use to present
-    /// content to the player. 
-    /// </para>
-    /// <para>
-    /// Subclasses of this abstract class may return subclasses of <see
-    /// cref="LocalizedLine"/>. For example, <see
-    /// cref="BuiltinLocalisedLineProvider"/> returns an <see
-    /// cref="AudioLocalizedLine"/>, which includes <see cref="AudioClip"/>;
-    /// views that make use of audio can then access this additional data.
-    /// </para>
+    /// information that is presented to the player through dialogue views.
     /// </remarks>
     /// <seealso cref="DialogueViewBase"/>
     public abstract class LineProviderBehaviour : MonoBehaviour, ILineProvider
     {
-        /// <summary>
-        /// Prepares and returns a <see cref="LocalizedLine"/> from the
-        /// specified <see cref="Yarn.Line"/>.
-        /// </summary>
-        /// <remarks>
-        /// This method should not be called if <see cref="LinesAvailable"/>
-        /// returns <see langword="false"/>.
-        /// </remarks>
-        /// <param name="line">The <see cref="Yarn.Line"/> to produce the <see
-        /// cref="LocalizedLine"/> from.</param>
-        /// <returns>A localized line, ready to be presented to the
-        /// player.</returns>
+        /// <inheritdoc/>
         public abstract YarnLineTask GetLocalizedLineAsync(Line line, CancellationToken cancellationToken);
 
-        /// <summary>
-        /// The YarnProject that contains the localized data for lines.
-        /// </summary>
-        /// <remarks>This property is set at run-time by the object that will be
-        /// requesting content (typically a <see cref="DialogueRunner"/>).
+        /// <inheritdoc/>
         public YarnProject? YarnProject { get; set; }
 
-        /// <summary>
-        /// Signals to the line provider that lines with the provided line IDs
-        /// may be presented shortly.        
-        /// </summary>
-        /// <remarks>
-        /// <para>
-        /// Subclasses of <see cref="LineProviderBehaviour"/> can override this
-        /// to prepare any neccessary resources needed to present these lines,
-        /// like pre-loading voice-over audio. The default implementation does
-        /// nothing.
-        /// </para>
-        /// <para style="info">
-        /// Not every line may run; this method serves as a way to give the line
-        /// provider advance notice that a line <i>may</i> run, not <i>will</i>
-        /// run.
-        /// </para>
-        /// <para>
-        /// When this method is run, the value returned by the <see
-        /// cref="LinesAvailable"/> property should change to false until the
-        /// necessary resources have loaded.
-        /// </para>
-        /// </remarks>
-        /// <param name="lineIDs">A collection of line IDs that the line
-        /// provider should prepare for.</param>
+        /// <inheritdoc/>
         public virtual YarnTask PrepareForLinesAsync(IEnumerable<string> lineIDs, CancellationToken cancellationToken)
         {
             // No-op by default.
             return YarnTask.CompletedTask;
         }
 
+        /// <inheritdoc/>
         public virtual bool LinesAvailable => true;
 
-        /// <summary>
-        /// Gets the user's current locale identifier, as a BCP-47 code.
-        /// </summary>
-        /// <remarks>
-        /// This value is used by the <see cref="DialogueRunner"/> to control
-        /// how certain replacement markers behave (for example, the
-        /// <c>[plural]</c> marker, which behaves differently depending on the
-        /// user's locale.)
-        /// </remarks>
+        /// <inheritdoc/>
         public abstract string LocaleCode { get; set; }
 
         /// <summary>
@@ -136,7 +167,10 @@ namespace Yarn.Unity
         {
         }
 
+        /// <inheritdoc/>
         public abstract void RegisterMarkerProcessor(string attributeName, IAttributeMarkerProcessor markerProcessor);
+
+        /// <inheritdoc/>
         public abstract void DeregisterMarkerProcessor(string attributeName);
     }
 
