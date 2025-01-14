a255537100303fd534a8b12793d459dea991e45d
Jon Manning <jon@secretlab.com.au>
2025-01-15 09:45:19 +1100

    Update character sample

--- Samples~/Characters/Dialogue/YarnVariables.cs
+++ Samples~/Characters/Dialogue/YarnVariables.cs
@@ -2,6 +2,46 @@ namespace MyGame {
 
     using Yarn.Unity;
 
+    /// <summary>
+    /// SwitchState
+    /// </summary>
+    /// <remarks>
+    /// Automatically generated from Yarn project at Assets/Samples/Characters/Dialogue/CharacterSample.yarnproject.
+    /// </remarks>
+    [System.CodeDom.Compiler.GeneratedCode("YarnSpinner", "3.0.0.0")]
+    public enum SwitchState {
+
+        /// <summary>
+        /// Off
+        /// </summary>
+        Off = 0,
+
+        /// <summary>
+        /// Mid
+        /// </summary>
+        Mid = 1,
+
+        /// <summary>
+        /// On
+        /// </summary>
+        On = 2,
+    }
+
+    [System.CodeDom.Compiler.GeneratedCode("YarnSpinner", "3.0.0.0")]
+    internal static class YarnVariablesTypeExtensions {
+        internal static int GetBackingValue(this SwitchState enumValue) {
+            switch (enumValue) {
+                    case SwitchState.Off:
+                        return 0;
+                    case SwitchState.Mid:
+                        return 1;
+                    case SwitchState.On:
+                        return 2;
+                    default:
+                throw new System.ArgumentException($"{enumValue} is not a valid enum case.");
+            }
+        }
+    }
     [System.CodeDom.Compiler.GeneratedCode("YarnSpinner", "3.0.0.0")]
     public partial class YarnVariables : Yarn.Unity.InMemoryVariableStorage, Yarn.Unity.IGeneratedVariableStorage {
         // Accessor for Number $times_interacted_with_switch
@@ -13,13 +53,15 @@ namespace MyGame {
             set => this.SetValue<float>("$times_interacted_with_switch", value);
         }
 
-        // Accessor for Bool $switch_on
-        /// <summary>
-        /// Whether the switch is currently turned on or off.
-        /// </summary>
-        public bool SwitchOn {
-            get => this.GetValueOrDefault<bool>("$switch_on");
-            set => this.SetValue<bool>("$switch_on", value);
+        // Accessor for SwitchState $switch_state
+        public SwitchState SwitchState {
+            get => this.GetEnumValueOrDefault<SwitchState>("$switch_state");
+            set => this.SetValue("$switch_state", value.GetBackingValue());
+        }
+
+        // Accessor for Bool $switch_on_or_mid
+        public bool SwitchOnOrMid {
+            get => this.GetValueOrDefault<bool>("$switch_on_or_mid");
         }
 
     }
