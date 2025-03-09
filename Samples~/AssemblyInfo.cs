b987cd3afea903f42ce04b72f1c7add1cf5066af
Jon Manning <jon@secretlab.com.au>
2025-03-09 14:16:07 +1100

    Update for beta release

--- Samples~/AssemblyInfo.cs
+++ Samples~/AssemblyInfo.cs
@@ -4,6 +4,6 @@ Yarn Spinner is licensed to you under the terms found in the file LICENSE.md.
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0-beta1")]
+[assembly: AssemblyVersion("3.0.0.235")]
+[assembly: AssemblyFileVersion("3.0.0.235")]
+[assembly: AssemblyInformationalVersion("3.0.0-beta2.Branch.release/v3.0.0-beta2.Sha.e95b5ff4c7576725366c01ba842f4b0a5e805c76")]
