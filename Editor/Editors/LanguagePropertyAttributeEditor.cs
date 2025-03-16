a6ce600a25acede4d4028eac939847a3216ce78d
Jon Manning <jon@secretlab.com.au>
2025-03-15 23:38:47 -0700

    Use PropertyScope in LanguageAttributeEditor

--- Editor/Editors/LanguagePropertyAttributeEditor.cs
+++ Editor/Editors/LanguagePropertyAttributeEditor.cs
@@ -16,47 +16,50 @@ namespace Yarn.Unity.Editor
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            // If this property is not a string, show an error label. (We
-            // can't call EditorGUI.PropertyField, because that would cause
-            // an infinite recursion - Unity would invoke this property
-            // drawer again.)
-            if (property.propertyType != SerializedPropertyType.String)
+            using (var scope = new EditorGUI.PropertyScope(position, label, property))
             {
-                EditorGUI.HelpBox(position, $"{property.name} is not a string.", MessageType.Error);
-                return;
-            }
-
-            // Display this property as a dropdown that lets you select a
-            // language.
-            var allCultures = Cultures.GetCultures().ToList();
-            var indices = Enumerable.Range(0, allCultures.Count());
-
-            var culturesToIndicies = allCultures.Zip(indices, (culture, index) => new { culture, index }).ToDictionary(pair => pair.culture.Name, pair => pair.index);
-
-            var value = property.stringValue;
-
-            int currentCultureIndex;
-
-            if (culturesToIndicies.ContainsKey(value))
-            {
-                currentCultureIndex = culturesToIndicies[value];
-            }
-            else
-            {
-                // The property doesn't contain a valid culture name. Show
-                // an 'empty' value, which will be replaced when the user
-                // selects a valid value from the dropdown.                
-                currentCultureIndex = -1;
-            }
-
-            var allCultureDisplayNames = allCultures.Select(c => (c.DisplayName + $":({c.Name})")).Select(n => new GUIContent(n)).ToArray();
-
-            using (var changeCheck = new EditorGUI.ChangeCheckScope())
-            {
-                var selectedIndex = EditorGUI.Popup(position, label, currentCultureIndex, allCultureDisplayNames);
-                if (changeCheck.changed)
+                // If this property is not a string, show an error label. (We
+                // can't call EditorGUI.PropertyField, because that would cause
+                // an infinite recursion - Unity would invoke this property
+                // drawer again.)
+                if (property.propertyType != SerializedPropertyType.String)
                 {
-                    property.stringValue = allCultures[selectedIndex].Name;
+                    EditorGUI.HelpBox(position, $"{property.name} is not a string.", MessageType.Error);
+                    return;
+                }
+
+                // Display this property as a dropdown that lets you select a
+                // language.
+                var allCultures = Cultures.GetCultures().ToList();
+                var indices = Enumerable.Range(0, allCultures.Count());
+
+                var culturesToIndicies = allCultures.Zip(indices, (culture, index) => new { culture, index }).ToDictionary(pair => pair.culture.Name, pair => pair.index);
+
+                var value = property.stringValue;
+
+                int currentCultureIndex;
+
+                if (culturesToIndicies.ContainsKey(value))
+                {
+                    currentCultureIndex = culturesToIndicies[value];
+                }
+                else
+                {
+                    // The property doesn't contain a valid culture name. Show
+                    // an 'empty' value, which will be replaced when the user
+                    // selects a valid value from the dropdown.                
+                    currentCultureIndex = -1;
+                }
+
+                var allCultureDisplayNames = allCultures.Select(c => (c.DisplayName + $":({c.Name})")).Select(n => new GUIContent(n)).ToArray();
+
+                using (var changeCheck = new EditorGUI.ChangeCheckScope())
+                {
+                    var selectedIndex = EditorGUI.Popup(position, label, currentCultureIndex, allCultureDisplayNames);
+                    if (changeCheck.changed)
+                    {
+                        property.stringValue = allCultures[selectedIndex].Name;
+                    }
                 }
             }
         }
