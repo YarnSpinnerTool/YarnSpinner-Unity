73656c9649b6a5b2357aaa94d42678d6f58cd3e5
Jon Manning <jon@secretlab.com.au>
2025-04-22 18:01:13 +1000

    Performance improvements for add-assets wizard; use string table from Yarn Project to identify assets; use same logic as builtin for finding assets

--- Editor/Editors/AddAssetsToAssetTableCollectionWizard.cs
+++ Editor/Editors/AddAssetsToAssetTableCollectionWizard.cs
@@ -2,6 +2,8 @@
 Yarn Spinner is licensed to you under the terms found in the file LICENSE.md.
 */
 
+#nullable enable
+
 namespace Yarn.Unity.Editor
 {
     using System.Collections;
@@ -13,19 +15,49 @@ namespace Yarn.Unity.Editor
     using UnityEditorInternal;
     using UnityEngine;
     using Yarn.Compiler;
+    using System;
 
 #if USE_UNITY_LOCALIZATION
     using UnityEditor.Localization;
     using UnityEngine.Localization.Tables;
-    using System;
+    using UnityEngine.Localization;
 
-    public class AddAssetsToAssetTableCollectionWizard : EditorWindow {
-        private AssetTableCollection assetTableCollection;
+    public class AddAssetsToAssetTableCollectionWizard : EditorWindow
+    {
 
-        private Type assetType = typeof(UnityEngine.AudioClip);
+        private YarnProject? yarnProject;
 
-        private Dictionary<string, DefaultAsset> localesToFolders = new Dictionary<string, DefaultAsset>();
-        private List<Type> allTypes;
+        private AssetTableCollection? assetTableCollection;
+
+        private Type? _assetType;
+        private Type AssetType
+        {
+            get
+            {
+                if (_assetType == null)
+                {
+                    var typeName = EditorPrefs.GetString("YarnSpinner_AddAssets_AssetType", string.Empty);
+                    if (typeName != string.Empty)
+                    {
+                        _assetType = System.Type.GetType(typeName, throwOnError: false, ignoreCase: false);
+                    }
+                }
+                _assetType ??= typeof(AudioClip);
+                return _assetType;
+            }
+            set
+            {
+                _assetType = value;
+                EditorPrefs.SetString("YarnSpinner_AddAssets_AssetType", _assetType?.AssemblyQualifiedName ?? string.Empty);
+            }
+        }
+
+        private Dictionary<string, DefaultAsset?> localesToFolders = new Dictionary<string, DefaultAsset?>();
+        private Type[] allTypes;
+        private GUIContent[] allTypeContents;
+        System.Collections.ObjectModel.ReadOnlyCollection<UnityEngine.Localization.Locale>? locales = null;
+
+        private Dictionary<LocaleIdentifier, LocalizationTable> _cachedTables = new();
 
         const string description = "This tool searches Asset Folders for assets of the selected type, and then adds them to an Asset Table Collection. Line Providers, like Unity Localised Line Provider, can then fetch these assets at run-time.";
 
@@ -37,77 +69,95 @@ namespace Yarn.Unity.Editor
             window.ShowPopup();
             window.titleContent = new GUIContent("Add Assets to Table Collection");
 
-            if (Selection.activeObject is AssetTableCollection collection) {
+            if (Selection.activeObject is AssetTableCollection collection)
+            {
                 window.assetTableCollection = collection;
             }
         }
 
-        void OnEnable() {
-            allTypes = TypeCache.GetTypesDerivedFrom<UnityEngine.Object>().OrderBy(t => t.Name).ToList();
+        void OnEnable()
+        {
+            allTypes = TypeCache.GetTypesDerivedFrom<UnityEngine.Object>().OrderBy(t => t.FullName).ToArray();
+            allTypeContents = allTypes.Select(t => new GUIContent(
+                        t.FullName.Replace(".", "/"),
+                        EditorGUIUtility.ObjectContent(null, t).image
+                    )).ToArray();
+            locales = LocalizationEditorSettings.GetLocales();
         }
 
         void OnGUI()
         {
 
-            bool readyToAddAssets = true;
-
             using (new GUILayout.VerticalScope())
             {
 
                 EditorGUILayout.BeginVertical(EditorStyles.inspectorFullWidthMargins);
-            
+
                 EditorGUILayout.HelpBox(description, MessageType.Info);
 
                 assetTableCollection = EditorGUILayout.ObjectField(new GUIContent("Asset Table Collection", "The asset table collection to add assets to"), assetTableCollection, typeof(AssetTableCollection), allowSceneObjects: false) as AssetTableCollection;
 
-                readyToAddAssets &= assetTableCollection != null;
 
-                
-                var selectedIndex = allTypes.IndexOf(assetType);
+                yarnProject = EditorGUILayout.ObjectField(new GUIContent("Yarn Project", "The Yarn Project to add assets for."), yarnProject, typeof(YarnProject), allowSceneObjects: false) as YarnProject;
 
-                using (var change = new EditorGUI.ChangeCheckScope()) {
-                    var newSelection = EditorGUILayout.Popup(new GUIContent("Asset Type", "The type of assets to find in the Asset Folders"), selectedIndex, allTypes.Select(t => new GUIContent(
-                        t.Name,
-                        EditorGUIUtility.ObjectContent(null, t).image
-                    )).ToArray());
+                var selectedIndex = Array.IndexOf(allTypes, AssetType);
 
-                    if (change.changed) {
-                        assetType = allTypes[newSelection];
+                using (var change = new EditorGUI.ChangeCheckScope())
+                {
+                    var newSelection = EditorGUILayout.Popup(new GUIContent("Asset Type", "The type of assets to find in the Asset Folders"), selectedIndex, allTypeContents);
+
+                    if (change.changed)
+                    {
+                        AssetType = allTypes[newSelection];
                     }
                 }
 
                 var headerStyle = EditorStyles.boldLabel;
 
-                EditorGUILayout.LabelField("Asset Folders", headerStyle);
-
-                EditorGUI.indentLevel += 1;
-
-                foreach (var locale in LocalizationEditorSettings.GetLocales())
+                if (assetTableCollection != null)
                 {
-                    LocalizationTable localizationTable = null;
+                    EditorGUILayout.LabelField("Asset Folders", headerStyle);
 
-                    if (assetTableCollection != null)
+                    EditorGUI.indentLevel += 1;
+
+                    if (locales == null)
                     {
-                        localizationTable = assetTableCollection.GetTable(locale.Identifier);
+                        EditorGUILayout.HelpBox("No locales were found in your project. Is Unity Localization installed and correctly configured?", MessageType.Error);
+                        return;
                     }
 
-                    using (new EditorGUI.DisabledGroupScope(localizationTable == null))
+                    foreach (var locale in locales)
                     {
-                        localesToFolders.TryGetValue(locale.Identifier.Code, out var currentFolder);
+                        if (_cachedTables.TryGetValue(locale.Identifier, out var localizationTable) == false || localizationTable == null)
+                        {
+                            if (assetTableCollection != null)
+                            {
+                                localizationTable = assetTableCollection.GetTable(locale.Identifier);
 
-                        localesToFolders[locale.Identifier.Code] = EditorGUILayout.ObjectField(
-                            new GUIContent(locale.LocaleName, $"The folder to search for {assetType.Name} assets for the '{locale.LocaleName}' locale"),
-                            currentFolder,
-                            typeof(DefaultAsset),
-                            allowSceneObjects: false) as DefaultAsset;
+                                _cachedTables[locale.Identifier] = localizationTable;
+                            }
+                        }
+
+                        if (localizationTable == null)
+                        {
+                            EditorGUILayout.LabelField(new GUIContent(locale.LocaleName), new GUIContent("No table in collection"));
+                            continue;
+                        }
+
+                        using (new EditorGUI.DisabledGroupScope(localizationTable == null))
+                        {
+                            localesToFolders.TryGetValue(locale.Identifier.Code, out var currentFolder);
+
+                            localesToFolders[locale.Identifier.Code] = EditorGUILayout.ObjectField(
+                                new GUIContent(locale.LocaleName, $"The folder to search for {AssetType.Name} assets for the '{locale.LocaleName}' locale"),
+                                currentFolder,
+                                typeof(DefaultAsset),
+                                allowSceneObjects: false) as DefaultAsset;
+                        }
                     }
+
+                    EditorGUI.indentLevel -= 1;
                 }
-
-                EditorGUI.indentLevel -= 1;
-
-                // We can only add assets if at least one folder has been provided.
-                readyToAddAssets &= localesToFolders.Where(kv => kv.Value != null).Count() > 0;
-
 
                 GUILayout.FlexibleSpace();
 
@@ -115,10 +165,13 @@ namespace Yarn.Unity.Editor
                 {
                     GUILayout.FlexibleSpace();
 
+                    var readyToAddAssets = assetTableCollection != null && yarnProject != null && localesToFolders.Where(kv => kv.Value != null).Count() > 0;
+
                     using (new EditorGUI.DisabledGroupScope(!readyToAddAssets))
                     {
-                        if (GUILayout.Button("Add Assets")) {
-                            AddAssets();
+                        if (GUILayout.Button("Add Assets"))
+                        {
+                            AddAssets(assetTableCollection!, yarnProject!, localesToFolders!, AssetType);
                             // this.Close();
                         }
                     }
@@ -128,8 +181,19 @@ namespace Yarn.Unity.Editor
             }
         }
 
-        private void AddAssets()
+        private static void AddAssets(AssetTableCollection assetTableCollection, YarnProject yarnProject, IReadOnlyDictionary<string, DefaultAsset> localesToFolders, System.Type assetType)
         {
+            var importer = AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(yarnProject)) as YarnProjectImporter;
+
+            if (importer == null)
+            {
+                throw new InvalidOperationException("Failed to get an importer for Yarn Project");
+            }
+
+            var stringTable = importer.GenerateStringsTable();
+
+            var lineIDs = stringTable.Select(e => e.ID);
+
             int totalCount = 0;
             foreach (var locale in LocalizationEditorSettings.GetLocales())
             {
@@ -151,11 +215,11 @@ namespace Yarn.Unity.Editor
 
                 var path = AssetDatabase.GetAssetPath(folder);
 
-                var allFiles = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories).Where(path => !path.EndsWith(".meta"));
+                var idsToAssetPaths = YarnProjectUtility.FindAssetPathsForLineIDs(lineIDs, AssetDatabase.GetAssetPath(folder), assetType);
 
-                foreach (var file in allFiles)
+                foreach (var (lineID, assetPath) in idsToAssetPaths)
                 {
-                    var asset = AssetDatabase.LoadAssetAtPath(file, assetType);
+                    var asset = AssetDatabase.LoadAssetAtPath(assetPath, assetType);
 
                     if (asset == null)
                     {
@@ -163,9 +227,7 @@ namespace Yarn.Unity.Editor
                         continue;
                     }
 
-                    var keyName = $"line:{Path.GetFileNameWithoutExtension(file)}";
-
-                    assetTableCollection.AddAssetToTable(locale.Identifier, keyName, asset);
+                    assetTableCollection.AddAssetToTable(locale.Identifier, lineID, asset);
 
                     perLocaleCount += 1;
                     totalCount += 1;
@@ -174,7 +236,7 @@ namespace Yarn.Unity.Editor
                 Debug.Log($"Added {perLocaleCount} assets to {locale.LocaleName}");
             }
             Debug.Log($"Added {totalCount} assets to asset table collection");
-                    
+
         }
     }
 #endif
