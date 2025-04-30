b7e60dc3071bc509a2f5db50a1b0cedc1254cf85
Jon Manning <jon@secretlab.com.au>
2025-04-30 16:32:35 +1000

    Fixup failing CodeAnalysisTests - Fix typo - Replace 'does the registration method get added' with 'do the actions get registered'

--- Tests/Editor/CodeAnalysisTests.cs
+++ Tests/Editor/CodeAnalysisTests.cs
@@ -8,6 +8,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
@@ -18,13 +19,42 @@ namespace Yarn.Unity.Tests
 {
     public class CodeAnalysisTests
     {
-        string outputFilePath => YarnTestUtility.TestFilesDirectoryPath + "YarnActionRegistration.cs";
         const string testScriptGUID = "32f15ac5211d54a68825dfb9532e93f4";
 
+        string OutputFilePath => YarnTestUtility.TestFilesDirectoryPath + "YarnActionRegistration.cs";
+
         string TestScriptPathSource => AssetDatabase.GUIDToAssetPath(testScriptGUID);
+
         string TestScriptPathInProject => YarnTestUtility.TestFilesDirectoryPath + Path.GetFileName(TestScriptPathSource);
 
         string TestNamespace => "Yarn.Unity.Generated." + TestContext.CurrentContext.Test.MethodName;
+
+        readonly string[] expectedCommands = new string[] {
+            "InstanceDemoActionWithNoName",
+            "instance_demo_action",
+            "instance_demo_action_with_params",
+            "instance_demo_action_with_optional_params",
+            "StaticDemoActionWithNoName",
+            "static_demo_action",
+            "static_demo_action_with_params",
+            "static_demo_action_with_optional_params",
+            "instance_variadic",
+            "static_variadic",
+        };
+
+        readonly string[] expectedFunctions = new string[] {
+            "direct_register_lambda_no_params",
+            "direct_register_lambda_fixed_params",
+            "direct_register_lambda_variadic_params",
+            "direct_register_method_no_params",
+            "direct_register_method_fixed_params",
+            "direct_register_method_variadic_params",
+            "int_void",
+            "int_params",
+            "local_constant_name",
+            "other_type_constant",
+            "constant_name",
+        };
 
         private void SetUpTestActionCode()
         {
@@ -55,7 +85,7 @@ namespace Yarn.Unity.Tests
                 var actions = analysis.GetActions();
                 var source = Yarn.Unity.ActionAnalyser.Analyser.GenerateRegistrationFileSource(actions, TestNamespace);
 
-                System.IO.File.WriteAllText(outputFilePath, source);
+                System.IO.File.WriteAllText(OutputFilePath, source);
 
                 AssetDatabase.Refresh();
                 yield return new RecompileScripts(expectScriptCompilation: true, expectScriptCompilationSuccess: true);
@@ -73,33 +103,6 @@ namespace Yarn.Unity.Tests
             var actions = analysis.GetActions();
             var generatedSource = ActionAnalyser.Analyser.GenerateRegistrationFileSource(actions, TestNamespace);
 
-            var expectedCommands = new string[] {
-                "InstanceDemoActionWithNoName",
-                "instance_demo_action",
-                "instance_demo_action_with_params",
-                "instance_demo_action_with_optional_params",
-                "StaticDemoActionWithNoName",
-                "static_demo_action",
-                "static_demo_action_with_params",
-                "static_demo_action_with_optional_params",
-                "instance_variadic",
-                "static_variadic",
-            };
-
-            var expectedFunctions = new string[] {
-                "direct_register_lambda_no_params",
-                "direct_register_lambda_fixed_params",
-                "direct_register_lambda_variadic_params",
-                "direct_register_method_no_params",
-                "direct_register_method_fixed_params",
-                "direct_register_method_variadic_params",
-                "int_void",
-                "int_params",
-                "local_constant_name",
-                "other_type_constant",
-                "constant_name",
-            };
-
             var commands = actions.Where(a => a.Type == ActionAnalyser.ActionType.Command);
             var functions = actions.Where(a => a.Type == ActionAnalyser.ActionType.Function);
 
@@ -114,43 +117,60 @@ namespace Yarn.Unity.Tests
             foreach (var functionName in expectedFunctions)
             {
                 functions.Should().Contain(c => c.Name == functionName, $"function {functionName} should be found");
-                var matchRegex = new Regex($@"RegisterFunctionDeclaration\(""{functionName}!""");
+                var matchRegex = new Regex($@"RegisterFunctionDeclaration\(""{functionName}""");
                 generatedSource.Should().Match(matchRegex, $"function {functionName} should be registered in the generated source");
             }
         }
 
+        private class TestActionRegistration : IActionRegistration
+        {
+            public List<string> RegisteredCommandNames = new();
+            public List<string> RegisteredFunctionNames = new();
+
+            public void AddCommandHandler(string commandName, Delegate handler) => RegisteredCommandNames.Add(commandName);
+
+            public void AddCommandHandler(string commandName, MethodInfo methodInfo) => RegisteredCommandNames.Add(commandName);
+
+            public void AddFunction(string name, Delegate implementation) { }
+
+            public void RegisterFunctionDeclaration(string name, Type returnType, Type[] parameterTypes) => RegisteredFunctionNames.Add(name);
+
+            public void RemoveCommandHandler(string commandName) { }
+
+            public void RemoveFunction(string name) { }
+        }
+
         [UnityTest]
-        public IEnumerator CodeAnalysis_GeneratedSourceCode_AttachesRegistrationMethod()
+        public IEnumerator CodeAnalysis_OnDomainReload_RegistersCommandsAndFunctions()
         {
             try
             {
+                // Given: a .cs file containing actions is added to the codebase
                 SetUpTestActionCode();
                 AssetDatabase.Refresh();
                 yield return new RecompileScripts(expectScriptCompilation: false, expectScriptCompilationSuccess: true);
 
                 var registrationMethods = Actions.ActionRegistrationMethods;
 
-                // The generated source code should have this fully qualified name:
-                string expectedFullMethodName = TestNamespace + ".ActionRegistration.RegisterActions";
+                var registrar = new TestActionRegistration();
 
-                var registrationMethodNames = registrationMethods.Select(m => GetFullMethodName(m.Method)).ToList();
-
-                Debug.Log($"Action registration methods:");
-                foreach (var registrationMethodName in registrationMethodNames)
+                // When
+                foreach (var method in registrationMethods)
                 {
-                    Debug.Log(registrationMethodName);
+                    method.Invoke(registrar, RegistrationType.Compilation);
                 }
 
-                Assert.Contains(expectedFullMethodName, registrationMethodNames, $"Actions should have registered method {expectedFullMethodName}");
-
-                string GetFullMethodName(System.Reflection.MethodInfo method)
+                // Then
+                foreach (var command in expectedCommands)
                 {
-                    if (method is null)
-                    {
-                        throw new ArgumentNullException(nameof(method));
-                    }
+                    registrar.RegisteredCommandNames.Should().ContainSingle(n => n == command,
+                        $"command {command} should be registered once");
+                }
 
-                    return $"{method.DeclaringType.FullName}.{method.Name}";
+                foreach (var function in expectedFunctions)
+                {
+                    registrar.RegisteredFunctionNames.Should().ContainSingle(n => n == function,
+                        $"function {function} should be registered once");
                 }
             }
             finally
