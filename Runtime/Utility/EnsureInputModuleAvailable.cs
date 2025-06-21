ada17a0f8c447c9d47cd939ad37e4656ade7e784
Jon Manning <jon@secretlab.com.au>
2025-06-21 11:37:47 +1000

    Don't write changes to disk if we're in a package

--- Runtime/Utility/EnsureInputModuleAvailable.cs
+++ Runtime/Utility/EnsureInputModuleAvailable.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+#nullable enable
+
 namespace Yarn.Unity
 {
 
@@ -40,20 +42,58 @@ namespace Yarn.Unity
                     // and the call running. Nothing to do.
                     return;
                 }
+                UnityEngine.EventSystems.BaseInputModule inputModule;
 
 #if ENABLE_INPUT_SYSTEM
                 // Create an input module that uses the Legacy Input Manager.
-                this.gameObject.AddComponent<UnityEngine.InputSystem.UI.InputSystemUIInputModule>();
+                inputModule = this.gameObject.AddComponent<UnityEngine.InputSystem.UI.InputSystemUIInputModule>();
 #elif ENABLE_LEGACY_INPUT_MANAGER
                 // Create an input module that uses the Input System.
-                this.gameObject.AddComponent<UnityEngine.EventSystems.StandaloneInputModule>();
+                inputModule = this.gameObject.AddComponent<UnityEngine.EventSystems.StandaloneInputModule>();
 #endif
+
+                // Next, determine whether this change is something that should
+                // be written to disk. We don't want to accidentally commit a
+                // change that inserts a specific type of input module, so we'll
+                // prevent any changes that happen in scenes that are located in
+                // Packages from being saved. (If you DO want to commit a change
+                // that adds an input module to a scene in Packages/, delete
+                // this auto-added input module from the game object and add one
+                // manually.)
+
+                bool isInPackages = IsInPackages(this.gameObject);
+                if (isInPackages)
+                {
+                    // This scene is in a package, and we should treat it as
+                    // read-only. Mark this component as not saved in the
+                    // editor.
+                    inputModule.hideFlags |= HideFlags.DontSaveInEditor;
+                }
+                else
+                {
+                    // Otherwise, we're free to save this change. Mark that the
+                    // game object has been modified.
+                    UnityEditor.EditorUtility.SetDirty(this.gameObject);
+                }
             };
 #endif
         }
 
-        private static bool IsInputModuleInScene =>
-            FindAnyObjectByType<UnityEngine.EventSystems.BaseInputModule>(FindObjectsInactive.Include) != null;
+        internal static bool IsInPackages(GameObject gameObject)
+        {
+            var scene = GameObject.GetScene(gameObject.GetInstanceID());
+            bool isInPackages = scene.path.StartsWith("Packages");
+            return isInPackages;
+        }
+
+        private bool IsInputModuleInScene
+        {
+            get
+            {
+                return this.TryGetComponent<UnityEngine.EventSystems.BaseInputModule>(out _)
+                    || FindAnyObjectByType<UnityEngine.EventSystems.BaseInputModule>(FindObjectsInactive.Include) != null;
+            }
+        }
     }
 
 #if UNITY_EDITOR
@@ -68,6 +108,28 @@ namespace Yarn.Unity
                 const string message = "This component checks to see if a UI Input Module exists in the scene. If one isn't available, it creates an input module on this object that's compatible with your current input system.";
 
                 EditorGUILayout.HelpBox(message, MessageType.Info);
+
+                if (target is not EnsureInputModuleAvailable module)
+                {
+                    return;
+                }
+
+                if (module.TryGetComponent<UnityEngine.EventSystems.BaseInputModule>(out var existingInputModule))
+                {
+                    if ((existingInputModule.hideFlags & HideFlags.DontSaveInEditor) != 0)
+                    {
+                        EditorGUILayout.Space();
+
+                        EditorGUILayout.HelpBox("The " + existingInputModule.GetType().Name + " on this object is marked as temporary, and won't be saved in the scene. Click the button below if you'd like to include it in the saved scene.", MessageType.Info);
+                        if (GUILayout.Button("Save in Scene"))
+                        {
+                            existingInputModule.hideFlags &= ~HideFlags.DontSaveInEditor;
+                            UnityEditor.EditorUtility.SetDirty(existingInputModule.gameObject);
+                            var scene = GameObject.GetScene(existingInputModule.gameObject.GetInstanceID());
+                            UnityEditor.SceneManagement.EditorSceneManager.SaveScene(scene);
+                        }
+                    }
+                }
             }
         }
     }
