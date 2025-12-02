62ebd18a9b86750f8b71ba10557fd8bb70a0077f
Jon Manning <jon@secretlab.com.au>
2025-12-03 07:45:18 +1100

    Update package version

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -5,9 +5,9 @@ Yarn Spinner is licensed to you under the terms found in the file LICENSE.md.
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("3.0.3.0")]
-[assembly: AssemblyFileVersion("3.0.3.0")]
-[assembly: AssemblyInformationalVersion("3.0.3.Branch.main.Sha.a906ced7bf2221568debb0626f8eee51b6a98b4c")]
+[assembly: AssemblyVersion("3.1.1.0")]
+[assembly: AssemblyFileVersion("3.1.1.0")]
+[assembly: AssemblyInformationalVersion("3.1.1+Branch.main.Sha.cb8fc541fc171ace1898785a98f08756c8bb5583")]
 [assembly: InternalsVisibleTo("YarnSpinnerTests")]
 [assembly: InternalsVisibleTo("YarnSpinner.Editor.Tests")]
 [assembly: InternalsVisibleTo("YarnSpinner.Unity.Editor")]
